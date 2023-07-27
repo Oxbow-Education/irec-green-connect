956facb79beed2782493c6e3cb4e4ea35b878d16
Nina Horne <nina.horne1221@gmail.com>
2023-07-27 12:52:24 -0400

    added Custom Plugins

---
+++ wp-content/plugins/irec-green-connect/includes/class-irec-green-connect-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://wherewego.org
+ * @since      1.0.0
+ *
+ * @package    Irec_Green_Connect
+ * @subpackage Irec_Green_Connect/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Irec_Green_Connect
+ * @subpackage Irec_Green_Connect/includes
+ * @author     WhereWeGo <support@wherewego.org>
+ */
+class Irec_Green_Connect_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
