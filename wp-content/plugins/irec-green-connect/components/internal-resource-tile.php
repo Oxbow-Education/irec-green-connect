332c80b09bdde42a3f2f13d46d5c779c152269a1
persis-randolph <persis.randolph@gmail.com>
2023-08-08 15:54:26 -0500

    add basing styling for resource cards

--- wp-content/plugins/irec-green-connect/components/internal-resource-tile.php
+++ wp-content/plugins/irec-green-connect/components/internal-resource-tile.php
@@ -1,12 +1,25 @@
 <?php
-
+  $tag_array = get_post_meta($post_id, 'worker_tags', true);
 ?>
-<div>
 
-  <p>Internal</p>
-  <h2><?php the_title() ?></h2>
-  <p>
-    <?php the_content() ?>
-
-  </p>
+<div class="internal-resource-tile resource-tile">
+  <div>
+    <?php the_post_thumbnail(); ?>
+      <div class="resource-tile-text">
+      <h5 class="resource-title"><?php the_title(); ?></h5>
+        <div>
+          <?php foreach ($tag_array as $tag) : ?>
+            <div class="resource-tag"><?php echo $tag ?></div>
+          <?php endforeach; ?>
+        </div>
+        <p class="resource-description">
+          [acf field="short_description" post_id="<?php echo get_the_ID() ?>"]
+        </p>
+      </div>
+  </div>
+  <div class="right">
+    <button class="resource-button-container read-more-button">
+      Read More
+    </button>
+  </div>
 </div>
