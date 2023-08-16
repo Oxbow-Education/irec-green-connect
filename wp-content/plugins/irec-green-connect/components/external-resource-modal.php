5713171a070235226da1bf00aa01ac6d733d449c
persis-randolph <persis.randolph@gmail.com>
2023-08-16 14:18:13 -0500

    add external modal style and functionality

--- wp-content/plugins/irec-green-connect/components/external-resource-modal.php
+++ wp-content/plugins/irec-green-connect/components/external-resource-modal.php
@@ -1,7 +1,9 @@
-<div class="external-resource-modal-bg" data-tag="modal-<?php echo get_the_id(); ?>-bg"></div>
-<div class="external-resource-modal" data-tag="modal-<?php echo get_the_id(); ?>">
-  <button class="close-modal">Close</button>
-  <h5>This is the Header</h5>
-  <p><?php echo get_the_id(); ?></p>
-  <p><a href="#">Link to external resource.</a></p>
+<div class="external-resource-modal-bg" data-tag="modal-<?php echo $post_id ?>-bg"></div>
+<div class="external-resource-modal" data-tag="modal-<?php echo $post_id ?>">
+  <button class="close-modal-btn" data-tag="<?php echo $post_id ?>"><img src="http://irec-green-connect.local/wp-content/uploads/2023/08/circle-xmark-light.png"></button>
+  <div class="modal-content">
+    <h5><?php the_title(); ?></h5>
+    <p><?php the_content(); ?></p>
+    <p><a target="_blank" href="<?php echo get_post_meta($post_id, 'url', true); ?>"><?php echo get_post_meta($post_id, 'url_text', true); ?></a></p>
+  </div>
 </div>
