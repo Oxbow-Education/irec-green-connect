472b889d945d9031af1f2acb89990088d20245da
persis-randolph <persis.randolph@gmail.com>
2023-09-27 10:16:55 -0500

    sync and changes wip

--- wp-content/plugins/irec-green-connect/components/top-resources.php
+++ wp-content/plugins/irec-green-connect/components/top-resources.php
@@ -9,6 +9,8 @@ if ($top_resources_query->have_posts()) {
     while ($top_resources_query->have_posts()) {
       $top_resources_query->the_post();
       // Display your post content here
+      // TODO: needs to display as carousel for more than 3 posts
+      // TODO: need to display as half card for external resource type posts
       include __DIR__ . '/internal-resource-tile.php';
     }
     wp_reset_postdata();
