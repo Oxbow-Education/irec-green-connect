5193cbd92bf22229bbb03fcfddfa6a5fc0108333
Nina Horne <nina@wherewego.org>
2023-10-16 14:03:26 -0600

    allow user to specify which filters are visible regardless of what's in data

--- wp-content/plugins/irec-green-connect/components/facet-buttons.php
+++ wp-content/plugins/irec-green-connect/components/facet-buttons.php
@@ -1,70 +1,47 @@
 <?php
-$tagType = $is_workers ? 'worker_tags' : 'organization_tags';
-$tags_query = new WP_Query(array(
-  'post_type' => 'post',
-  'posts_per_page' => -1,
-  'meta_query' => array(
-    array(
-      'key' => $tagType,
-      'value' => '',
-      'compare' => '!='
-    )
-  ),
-  'orderby' => 'meta_value',
-  'order' => 'ASC',
-  'fields' => 'ids', // gets back an array of the post_ids only
-));
+$page_id = get_the_ID(); // Get current page ID
+
+// Fetch the custom field values for the current page
+$worker_filters_to_show = get_post_meta($page_id, 'worker_filters_to_show', true);
+$org_filters_to_show = get_post_meta($page_id, 'org_filters_to_show', true);
+$user_filters_to_show = get_post_meta($page_id, 'user_filters_to_show', true);
 
 $tags = array();
 
-foreach ($tags_query->posts as $post_id) {
-  $tags_array = array();
-  // URL path contains /organizations
-  if (strpos($_SERVER['REQUEST_URI'], '/organizations') !== false) {
-    $org_tag_array = get_post_meta($post_id, 'organization_tags', true);
-    $who_for_tag_array = get_post_meta($post_id, 'who_is_this_for', true);
-    if (is_array($who_for_tag_array)) {
-      // Exclude "Worker User" tag from the array
-      $tags_array = array_merge($tags_array, array_filter($who_for_tag_array, function ($tag) {
-        return $tag !== 'Worker User';
-      }));
-    } elseif (!empty($who_for_tag_array) && $who_for_tag_array !== 'Worker User') {
-      // Convert the string value to an array if it has content and is not "Worker User"
-      $tags_array[] = $who_for_tag_array;
-    }
+// URL path contains /organizations
+if (strpos($_SERVER['REQUEST_URI'], '/organizations') !== false) {
+  if (is_array($org_filters_to_show)) {
+    $tags = $org_filters_to_show;
   }
-  // URL path contains /workers
-  elseif (strpos($_SERVER['REQUEST_URI'], '/workers') !== false) {
-    $worker_tag_array = get_post_meta($post_id, 'worker_tags', true);
-    if (is_array($worker_tag_array)) {
-      $tags_array = array_merge($tags_array, $worker_tag_array);
-    } elseif (!empty($worker_tag_array)) {
-      // Convert the string value to an array if it has content
-      $tags_array[] = $worker_tag_array;
-    }
-  }
-
-  if (!empty($tags_array)) {
-    $tags = array_merge($tags, $tags_array);
+  if (is_array($user_filters_to_show)) {
+    // Exclude "Worker User" tag from the array
+    $tags = array_merge($tags, array_filter($user_filters_to_show, function ($tag) {
+      return $tag !== 'Worker User';
+    }));
   }
 }
-  
+// URL path contains /workers
+elseif (strpos($_SERVER['REQUEST_URI'], '/workers') !== false) {
+  if (is_array($worker_filters_to_show)) {
+    $tags = $worker_filters_to_show;
+  }
+}
+
 // Remove duplicates and reindex the array
 $tags = array_values(array_unique($tags));
 sort($tags);
-
 
 ?>
 <!-- Facet buttons -->
 <div class="facet-buttons">
   <h4 id="filter-by">Filter By</h4>
   <?php
-    if (strpos($_SERVER['REQUEST_URI'], '/organizations') !== false) {
-      foreach ($org_tag_array as $orgTag) : ?>
-        <button class="facet-button" data-tag="<?php echo esc_attr($orgTag); ?>"><?php echo esc_html($orgTag); ?></button>
-      <?php endforeach;
-    }
-    echo '<div class="spacer"></div>';
+  if (strpos($_SERVER['REQUEST_URI'], '/organizations') !== false && !empty($org_filters_to_show)) {
+    foreach ($org_filters_to_show as $orgTag) : ?>
+      <button class="facet-button" data-tag="<?php echo esc_attr($orgTag); ?>"><?php echo esc_html($orgTag); ?></button>
+    <?php endforeach;
+  }
+  echo '<div class="spacer"></div>';
   foreach ($tags as $tag) : ?>
     <button class="facet-button <?php if (strpos($_SERVER['REQUEST_URI'], '/organizations') !== false) echo 'org-tag'; ?>" data-tag="<?php echo esc_attr($tag); ?>"><?php echo esc_html($tag); ?></button>
   <?php endforeach; ?>
