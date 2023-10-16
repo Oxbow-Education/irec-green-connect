ca464e5c72134ed9b8aaaf14e974d05f3efc2e1f
Nina Horne <nina@wherewego.org>
2023-10-16 14:33:05 -0600

    filters on workers and organizational resources

--- wp-content/plugins/irec-green-connect/components/external-resource-tile.php
+++ wp-content/plugins/irec-green-connect/components/external-resource-tile.php
@@ -1,9 +1,8 @@
 <?php
-  $post_id = get_the_ID();
-  $tag_array = $is_workers ?  get_post_meta($post_id, 'worker_tags', true) : get_post_meta($post_id, 'organization_tags', true);
-  // $who_is_this_for_tags = !$is_workers ? get_post_meta($post_id, 'who_is_this_for', true) : array();
-  $post_text = get_post_meta($post_id, 'short_description', true);
-  require __DIR__ . '/external-resource-modal.php';
+$post_id = get_the_ID();
+$tag_array = $is_workers ?  get_post_meta($post_id, 'worker_tags', true) : get_post_meta($post_id, 'organization_tags', true);
+$post_text = get_post_meta($post_id, 'short_description', true);
+require __DIR__ . '/external-resource-modal.php';
 ?>
 <div class="external-resource-tile resource-tile" data-tag="<?php echo $post_id; ?>">
   <div class="resource-tile-text">
@@ -12,14 +11,16 @@
       <?php if (is_array($tag_array)) {
         foreach ($tag_array as $tag) : ?>
           <div class="resource-tag"><?php echo $tag; ?></div>
-      <?php endforeach;
+        <?php endforeach;
       }
       // this is code to show when testing org tag filtering - probably not displaying these tags?
+      $who_is_this_for_tags = get_post_meta($post_id, 'who_is_this_for', true);
+
       if (is_array($who_is_this_for_tags)) {
         foreach ($who_is_this_for_tags as $tag) : ?>
-          <!-- <div class="resource-tag org-tag"><?php echo $tag; ?></div> -->
+          <div class="resource-tag org-tag"><?php echo $tag; ?></div>
       <?php endforeach;
-      }?>
+      } ?>
     </div>
 
     <p class="resource-description clamp-2">
