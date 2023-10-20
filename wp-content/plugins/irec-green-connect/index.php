b944bf09e570dd333293c3a75b53bd014a9d9b38
Nina Horne <nina@wherewego.org>
2023-10-20 14:14:38 -0600

    wage data shortcode and styling

--- wp-content/plugins/irec-green-connect/index.php
+++ wp-content/plugins/irec-green-connect/index.php
@@ -210,7 +210,7 @@ function wage_data_widget()
   wp_enqueue_style('wage-data-widget-css', '/wp-content/plugins/irec-green-connect/public/css/wage-data-widget.css');
   wp_enqueue_script('wage-data-widget-js', '/wp-content/plugins/irec-green-connect/public/js/wage-data-widget.js');
 
-  include __DIR__ . '/components/wage-data-widgets.php';
+  include __DIR__ . '/components/wage-data-widget.php';
   return ob_get_clean();
 }
 add_shortcode('wage_data_widget', 'wage_data_widget');
