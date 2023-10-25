a5e0ff5eafac7f78c261461a1120158da05300c0
Nina Horne <nina@wherewego.org>
2023-10-24 22:32:03 -0500

    updates

---
+++ wp-content/plugins/wpsynchro/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '10d9ec883578d7a001c7f00baf4276c8474d0399',
+        'name' => 'wpsynchro/wpsynchro',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wpsynchro/wpsynchro' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '10d9ec883578d7a001c7f00baf4276c8474d0399',
+            'dev_requirement' => false,
+        ),
+    ),
+);
