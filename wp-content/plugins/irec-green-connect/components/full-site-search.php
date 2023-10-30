0f0edb3070a148bb60b74a53e4cf94407771f623
Nina Horne <nina@wherewego.org>
2023-10-30 18:07:39 -0400

    header and footer complete

---
+++ wp-content/plugins/irec-green-connect/components/full-site-search.php
@@ -0,0 +1,6 @@
+<form id="full-site-searchbox" class="hidden">
+  <input type=" text" id="fullSiteSearch" name="search" placeholder="Enter keyword">
+  <div id="fullSearchHits"></div>
+  <p id="no-results-message" style="display: none">No results.
+  </p>
+</form>
