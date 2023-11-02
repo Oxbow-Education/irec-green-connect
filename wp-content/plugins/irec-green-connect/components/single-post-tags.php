1a6cde2a87dfa20e0549b79b44b1f075da8de12e
Nina Horne <nina@wherewego.org>
2023-11-02 12:49:19 -0400

    resources update

--- wp-content/plugins/irec-green-connect/components/single-post-tags.php
+++ wp-content/plugins/irec-green-connect/components/single-post-tags.php
@@ -1,29 +1,33 @@
 <!-- single resource page tags -->
 <?php
-  $worker_tag_array = get_post_meta(get_the_ID(), 'worker_tags', true);
-  $org_tag_array = get_post_meta(get_the_ID(), 'organization_tags', true);
-  $who_for_tag_array = get_post_meta(get_the_ID(), 'who_is_this_for', true);
-?>
+$worker_tag_array = get_post_meta(get_the_ID(), 'worker_tags', true);
+$org_tag_array = get_post_meta(get_the_ID(), 'organization_tags', true);
+$who_for_tag_array = get_post_meta(get_the_ID(), 'who_is_this_for', true);
+
+// Remove "Worker User" from the array
+$who_for_tag_array = array_filter($who_for_tag_array, function ($value) {
+  return $value !== 'Worker User';
+}); ?>
 <div class="single-resource-tags">
   <?php
-    // tags - green for org related, blue for worker
-    // who is this for tags
-    if (is_array($who_for_tag_array)) {
-      foreach ($who_for_tag_array as $who_for_tag) : ?>
-        <div class="resource-tag <?php if ($who_for_tag != "Worker User") echo "org-tag"; ?>"><?php echo $who_for_tag ?></div>
-      <?php endforeach;
-    }
-    // worker tags if they exist
-    if (is_array($worker_tag_array)) {
-      foreach ($worker_tag_array as $worker_tag) : ?>
-        <div class="resource-tag"><?php echo $worker_tag ?></div>
-      <?php endforeach;
-    }
-    // org tags if they exist
-    if (is_array($org_tag_array)) {
-      foreach ($org_tag_array as $org_tag) : ?>
-        <div class="resource-tag org-tag"><?php echo $org_tag ?></div>
-      <?php endforeach;
-    }
+  // tags - green for org related, blue for worker
+  // who is this for tags
+  if (is_array($who_for_tag_array)) {
+    foreach ($who_for_tag_array as $who_for_tag) : ?>
+      <div class="resource-tag <?php if ($who_for_tag != "Worker User") echo "org-tag"; ?>"><?php echo $who_for_tag ?></div>
+    <?php endforeach;
+  }
+  // worker tags if they exist
+  if (is_array($worker_tag_array)) {
+    foreach ($worker_tag_array as $worker_tag) : ?>
+      <div class="resource-tag"><?php echo $worker_tag ?></div>
+    <?php endforeach;
+  }
+  // org tags if they exist
+  if (is_array($org_tag_array)) {
+    foreach ($org_tag_array as $org_tag) : ?>
+      <div class="resource-tag org-tag"><?php echo $org_tag ?></div>
+  <?php endforeach;
+  }
   ?>
 </div>
