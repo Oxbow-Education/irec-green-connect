0c69b8d13c6c86442611c14debb816509841e5e9
Nina Horne <nina.horne1221@gmail.com>
2023-11-27 16:48:00 -0600

    udpated placeholder

--- wp-content/plugins/irec-green-connect/components/full-site-search.php
+++ wp-content/plugins/irec-green-connect/components/full-site-search.php
@@ -1,5 +1,5 @@
 <form id="full-site-searchbox" action="" class="hidden">
-  <input type="text" id="fullSiteSearch" name="search" placeholder="Enter keyword">
+  <input type="text" id="fullSiteSearch" name="search" placeholder="Search for a page or resource">
   <div id="fullSearchHits"></div>
   <p id="no-results-message" style="display: none">No results.
   </p>
