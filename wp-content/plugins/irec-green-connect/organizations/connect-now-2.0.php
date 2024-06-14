311a6bf1df6068d97061a54be4891fc5b7635531
Nina Horne <nina@wherewego.org>
2024-06-14 17:14:10 -0500

    search clear show/hide

--- wp-content/plugins/irec-green-connect/organizations/connect-now-2.0.php
+++ wp-content/plugins/irec-green-connect/organizations/connect-now-2.0.php
@@ -39,7 +39,7 @@
         <div class="more-filters__container"></div>
       </div>
       <form id="algoliaSearch" class="search">
-        <input class="search__input" name="query" type="text" placeholder="Search by Program or Oragnization">
+        <input class="search__input" name="query" required type="text" placeholder="Search by Program or Oragnization">
         <button type="button" class="search__clear"><img src="/wp-content/plugins/irec-green-connect/public/img/times-2.png" alt="clear search"></button>
         <button type="submit" class="search__icon"><img src="/wp-content/plugins/irec-green-connect/public/img/magnifying-glass.svg" alt="submit search"></button>
       </form>
