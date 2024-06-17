96aa230a66bf8c0dfae00b6f8502e30fe9bc5803
Nina Horne <nina@wherewego.org>
2024-06-17 17:53:49 -0500

    include remote toggle styled properly

--- wp-content/plugins/irec-green-connect/organizations/connect-now-2.0.php
+++ wp-content/plugins/irec-green-connect/organizations/connect-now-2.0.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/themes/light.css" />
 <script type="module" src="https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/shoelace-autoloader.js"></script>
 
 <div class="connect-now">
@@ -60,23 +59,50 @@
     </div>
     <div id="map" class="map"></div>
   </div>
-</div>
 
-<div class="mobile-filters">
-  <button id="mapView">
-    <img src="/wp-content/plugins/irec-green-connect/public/img/map-icon.png" />
-    Map View</button>
-  <button id="listView" class="hide">
-    <img src="/wp-content/plugins/irec-green-connect/public/img/list-view.png" />
-    List View</button>
-  <div class="mobile-filters__divider"></div>
-  <button id="drawerButton">
-    <img src="/wp-content/plugins/irec-green-connect/public/img/filters-icon.png" />
-    Filters</button>
-</div>
-<sl-drawer id="drawer" label="Drawer" placement="bottom" class="drawer-placement-bottom">
-  This drawer slides in from the bottom.
-  <sl-button slot="footer" variant="primary">Close</sl-button>
-</sl-drawer>
+  <div class="mobile-filters">
+    <button id="mapView">
+      <img src="/wp-content/plugins/irec-green-connect/public/img/map-icon.png" />
+      Map View</button>
+    <button id="listView" class="hide">
+      <img src="/wp-content/plugins/irec-green-connect/public/img/list-view.png" />
+      List View</button>
+    <div class="mobile-filters__divider"></div>
+    <button id="drawerButton">
+      <img src="/wp-content/plugins/irec-green-connect/public/img/filters-icon.png" />
+      Filters</button>
+  </div>
+  <sl-drawer id="drawer" label="Filter your results" placement="bottom" style="--size: 90vh;" class="drawer-placement-bottom">
+    <sl-switch style="--height: 23.81px; --width: 35.72px;" class="switch">Include Remote</sl-switch>
+
+    <h3>Opportunity</h3>
+    <sl-checkbox>Hiring</sl-checkbox>
+    <p>Get hired for a job or apprenticeship. </p>
+    <sl-checkbox>Training</sl-checkbox>
+    <p>Get training for a career role. </p>
+    <sl-checkbox>Information</sl-checkbox>
+    <p>Learn more about the energy workforce. </p>
+    <sl-checkbox>Bids & Contracts</sl-checkbox>
+    <p>If you’re a contractor, you can find potential customers.</p>
+    <sl-checkbox>Create an Apprenticeship Program</sl-checkbox>
+    <p>If you’re an employer, you can find potential partners.</p>
+    <h3>Tags</h3>
+    <button>Community Partner</button>
+    <button>Electric Vehicles & Battery Storage</button>
+    <button>Energy Efficiency</button>
+    <button>Registered Apprenticeship</button>
+    <button>Solar Energy</button>
+    <button>Wind Energy</button>
+    <button>Training Provider</button>
+    <button>Weatherization Assistance Program Employer</button>
+    <button>Youth Program</button>
+    <h3>Careers</h3>
+    <form id="algoliaSearch" class="search">
+      <input class="search__input" name="query" required type="text" placeholder="Search by Program or Oragnization">
+      <button type="button" class="search__clear"><img src="/wp-content/plugins/irec-green-connect/public/img/times-2.png" alt="clear search"></button>
+      <button type="submit" class="search__icon"><img src="/wp-content/plugins/irec-green-connect/public/img/magnifying-glass.svg" alt="submit search"></button>
+    </form>
+    <sl-button slot="footer" variant="primary">Close</sl-button>
+  </sl-drawer>
 
 </div>
