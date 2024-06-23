a752373a229d20bbbb72d091f2ca6368d514eb8b
Nina Horne <nina@wherewego.org>
2024-06-23 14:08:51 -0500

    external resource functionality

--- wp-content/plugins/irec-green-connect/resources/resources-search.js
+++ wp-content/plugins/irec-green-connect/resources/resources-search.js
@@ -125,6 +125,8 @@ function generateExternalResourceHTML(item) {
 
 function syncAlgoliaWithURL(properties) {
   const url = new URL(window.location);
+  const resource = url.searchParams.get('resource');
+  if (resource) return;
   let facetFilters = [];
   properties.forEach((property) => {
     const { facet, paramValue } = property;
@@ -181,7 +183,7 @@ function generateModalHTML(resource) {
   const html = `
 
 <div class="external-resource-modal active" data-tag="${resource.objectId}">
-<button class="external-resource-modal-close"> <svg class="close-modal-svg" xmlns="http://www.w3.org/2000/svg" width="42.762" height="42.762" viewBox="0 0 42.762 42.762">
+<button onclick="closeModal()" class="external-resource-modal-close"> <svg class="close-modal-svg" xmlns="http://www.w3.org/2000/svg" width="42.762" height="42.762" viewBox="0 0 42.762 42.762">
 <path class="a" d="M15.092,15.092a1.326,1.326,0,0,1,1.888,0l4.4,4.4,4.4-4.4a1.335,1.335,0,1,1,1.888,1.888l-4.4,4.4,4.4,4.4a1.335,1.335,0,1,1-1.888,1.888l-4.4-4.4-4.4,4.4a1.335,1.335,0,1,1-1.888-1.888l4.4-4.4-4.4-4.4A1.326,1.326,0,0,1,15.092,15.092Zm27.67,6.289A21.381,21.381,0,1,1,21.381,0,21.379,21.379,0,0,1,42.762,21.381ZM21.381,2.673A18.708,18.708,0,1,0,40.089,21.381,18.71,18.71,0,0,0,21.381,2.673Z" />
 </svg></button>
   
@@ -194,4 +196,12 @@ function generateModalHTML(resource) {
 </div>`;
 
   return html;
+}
+
+function closeModal() {
+  updateQueryParam('resource', '', true, true);
+  const externalResourceModal = document.getElementById(
+    'externalResourceModal',
+  );
+  externalResourceModal.hide();
 }
