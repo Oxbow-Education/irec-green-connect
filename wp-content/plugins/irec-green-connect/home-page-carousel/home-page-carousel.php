80164d49dbb4221ee7a003e7d3558cab5e4b4194
Nina Horne <nina@wherewego.org>
2024-06-23 22:38:39 -0500

    desktop done

--- wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.php
+++ wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.php
@@ -2,7 +2,7 @@
   <div class="carousel">
     <div class="slide active slide-1">
       <div class="slide-image">
-        <img class="slide-image" src="/wp-content/plugins/irec-green-connect/public/img/for-job-seekers.png" />
+        <img class="slide-image" src="/wp-content/plugins/irec-green-connect/public/img/slide-1.png" />
         <h2>For Job Seekers</h2>
 
       </div>
@@ -36,51 +36,108 @@
         </div>
       </div>
     </div>
-    <div class="slide left">
-      <img src="/wp-content/plugins/irec-green-connect/public/img/slide-2.png" />
+    <div class="slide left slide-2">
+      <div class="slide-image">
+        <img class="slide-image" src="/wp-content/plugins/irec-green-connect/public/img/slide-2.png" />
+        <h2>For Employers</h2>
+
+      </div>
       <div class="slide-links">
-        <a class="link--yellow" href=""><strong>Find Organizations</strong> for Clean Energy
-          Apprenticeships and Training Opportunities
+
+        <div class="link link--yellow row-1-4">
+          <p>
+            Partner with training programs in your area
+          </p>
+
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Learn More about Green Workforce Careers
+        </div>
+        <div class="link link--blue row-1-3">
+          <p>
+            Start an apprenticeship program
+          </p>
+
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Find contractor-specific marketing, educational, and industry resources in our Resources Hub for Contractors
+        </div>
+        <div class="link link--green row-4-6">
+          <p>
+            Access workforce development tools and templates
+          </p>
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
+        </div>
+        <div class="link link--light-blue row-2-6">
+          <p>
+            Learn how to hire and retain a diverse workforce
+          </p>
+          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
+        </div>
       </div>
     </div>
-    <!-- <div class="slide back">
-      <img src="/wp-content/plugins/irec-green-connect/public/img/slide-3.png" />
+    <div class="slide back slide-3">
+      <div class="slide-image">
+        <img class="slide-image" src="/wp-content/plugins/irec-green-connect/public/img/slide-3.png" />
+        <h2>For Education &
+          Workforce Organizations</h2>
+
+      </div>
       <div class="slide-links">
-        <a class="link--yellow" href="">Learn how to participate in weatherization work
-          and other green energy opportunities
+
+        <div class="link link--yellow row-1-4">
+          <p>
+            Connect with employers and training providers in your area
+          </p>
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Learn More about Green Workforce Careers
+        </div>
+        <div class="link link--blue row-1-3">
+          <p>
+            Start or join an apprenticeship program
+          </p>
+
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Find contractor-specific marketing, educational, and industry resources in our Resources Hub for Contractors
+        </div>
+        <div class="link link--green row-4-6">
+          <p>
+            Help students and clients explore career opportunities
+          </p>
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
+        </div>
+        <div class="link link--light-blue row-2-6">
+          <p>
+            Learn about IREC accreditation and certification
+          </p>
+          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
+        </div>
       </div>
     </div>
-    <div class="slide right">
-      <img src="/wp-content/plugins/irec-green-connect/public/img/slide-4.png" />
-      <div class="slide-links">
-        <a href="">Learn how to participate in weatherization work
-          and other green energy opportunities
-          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Learn More about Green Workforce Careers
-          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Find contractor-specific marketing, educational, and industry resources in our Resources Hub for Contractors
-          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
+    <div class="slide right slide-3">
+      <div class="slide-image">
+        <img class="slide-image" src="/wp-content/plugins/irec-green-connect/public/img/slide-3.png" />
+        <h2>For Contractors</h2>
+
       </div>
-    </div> -->
+      <div class="slide-links">
+
+        <div class="link link--yellow row-1-4">
+          <p>
+            Connect with organizations seeking contractor
+          </p>
+          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
+        </div>
+        <div class="link link--blue row-1-3">
+          <p>
+            Access the Weatherization Contractor’s Guide to Success
+          </p>
+
+          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
+        </div>
+
+        <div class="link link--light-blue row-2-6">
+          <p>
+            Explore opportunities in home energy performance
+          </p>
+          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
+        </div>
+      </div>
+    </div>
     <div class="carousel__navigation">
       <button class="navigation navigation--left">
         <img src="/wp-content/plugins/irec-green-connect/public/img/chevron-left-sharp-solid.png" />
