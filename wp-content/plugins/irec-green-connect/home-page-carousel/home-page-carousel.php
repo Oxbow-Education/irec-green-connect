3dd72701e98dd8d012c0f512ddbabce895ab6b69
Nina Horne <nina@wherewego.org>
2024-06-23 21:54:23 -0500

    first slide

--- wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.php
+++ wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.php
@@ -1,25 +1,41 @@
 <div class="home-page-carousel">
   <div class="carousel">
-    <div class="slide active">
-      <img src="/wp-content/plugins/irec-green-connect/public/img/slide-1.png" />
+    <div class="slide active slide-1">
+      <img class="slide-image" src="/wp-content/plugins/irec-green-connect/public/img/for-job-seekers.png" />
       <div class="slide-links">
-        <a href="">Learn how to participate in weatherization work
-          and other green energy opportunities
+        <button class="link link--yellow row-1-4">
+          <p>
+            Connect with employers who are hiring
+          </p>
+
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Learn More about Green Workforce Careers
+        </button>
+        <button class="link link--blue row-1-3">
+          <p>
+            Explore education, training, and apprenticeship programs
+          </p>
+
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
-        <a href="">Find contractor-specific marketing, educational, and industry resources in our Resources Hub for Contractors
+        </button>
+        <button class="link link--green row-4-6">
+          <p>
+            Learn more about career opportunities
+          </p>
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
-        </a>
+        </button>
+        <button class="link link--light-blue row-2-6">
+          <p>
+            Explore careers in building performance
+          </p>
+          <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
+        </button>
       </div>
     </div>
-    <div class="slide left">
+    <!-- <div class="slide left">
       <img src="/wp-content/plugins/irec-green-connect/public/img/slide-2.png" />
       <div class="slide-links">
-        <a href="">Learn how to participate in weatherization work
-          and other green energy opportunities
+        <a class="link--yellow" href=""><strong>Find Organizations</strong> for Clean Energy
+          Apprenticeships and Training Opportunities
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
         </a>
         <a href="">Learn More about Green Workforce Careers
@@ -33,7 +49,7 @@
     <div class="slide back">
       <img src="/wp-content/plugins/irec-green-connect/public/img/slide-3.png" />
       <div class="slide-links">
-        <a href="">Learn how to participate in weatherization work
+        <a class="link--yellow" href="">Learn how to participate in weatherization work
           and other green energy opportunities
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
         </a>
@@ -59,7 +75,7 @@
           <img src="/wp-content/plugins/irec-green-connect/public/img/arrow-right.png" />
         </a>
       </div>
-    </div>
+    </div> -->
     <div class="carousel__navigation">
       <button class="navigation navigation--left">
         <img src="/wp-content/plugins/irec-green-connect/public/img/chevron-left-sharp-solid.png" />
