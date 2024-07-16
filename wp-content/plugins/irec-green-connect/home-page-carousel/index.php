4eb63a7d5f7d9ee382d833e852fb4c98224cee48
Nina Horne <nina@wherewego.org>
2024-07-16 09:29:50 -0500

    scopes

--- wp-content/plugins/irec-green-connect/home-page-carousel/index.php
+++ wp-content/plugins/irec-green-connect/home-page-carousel/index.php
@@ -10,7 +10,7 @@ function home_page_carousel_2_0_shortcode_function()
   wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css');
   wp_enqueue_script('swiper', 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js');
   wp_enqueue_style('shoelace-css', 'https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.15.1/cdn/themes/light.css');
-  wp_enqueue_style('home-page-carousel-css', "/wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.css", array(), '2.0.5');
+  wp_enqueue_style('home-page-carousel-css', "/wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.css", array(), '2.0.6');
   wp_enqueue_script('home-page-carousel-js', '/wp-content/plugins/irec-green-connect/home-page-carousel/home-page-carousel.js', array(), '2.0.5');
   wp_enqueue_style('home-page-carousel-css', '/wp-content/plugins/irec-green-connect/public/css/home-page-carousel.css', array(), '2.0.5');
   // Return the output
